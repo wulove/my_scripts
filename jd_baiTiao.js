b403e057e3f5bc495de36ee8201c9feadf9d18ef
wulove <iwulove@gmail.com>
2021-02-01 14:53:56 +0800

    update

--- jd_baiTiao.js
+++ jd_baiTiao.js
@@ -58,7 +58,7 @@ let prize =
       let date = new Date($.time("yyyy/MM/dd HH:mm:ss"));
       cookieExpire = false;
       lackCoin = false;
-      await queryCouponsNotGroup()
+      //await queryCouponsNotGroup()
       console.log(`\n********`+cookieExpire +`*********\n`);
       if (cookieExpire) {
         $.msg($.name, '提示：请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
