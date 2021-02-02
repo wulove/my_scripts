fd955b415c69a0373fc0dc9966e99b3e3e4c6367
wulove <iwulove@gmail.com>
2021-02-02 10:05:04 +0800

    update

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -35,9 +35,9 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '',
+  '4vvbjlml6tdfc4ithg6rz56qpyaap4kvcofak2i',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '',
+  'otcv6vmocc5iix6e4yd2i5hrze5ac3f4ijdgqji',
 ]
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
