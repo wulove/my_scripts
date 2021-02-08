7f23efb73ff27328425acc5dbb3c97626836cef0
wulove <iwulove@gmail.com>
2021-02-08 11:24:09 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,31 +4,11 @@
 ##############短期活动##############
 # 年货节(活动时间：2021年1月9日-2021年2月9日)
 10 8 * * * node /scripts/jd_nh.js >> /scripts/logs/jd_nh.log 2>&1
-# 京东炸年兽集爆竹(活动时间:2021-1-18至2021-2-11)怕有遗漏故多运行几次
-0 * * * * node /scripts/jd_nian.js >> /scripts/logs/jd_nian.log 2>&1
-# 专门收集每秒产生的爆竹(1小时运行一次)
-30 * * * * node /scripts/jd_nianCollect.js >> /scripts/logs/jd_nianCollect.log 2>&1
-# 京东炸年兽签到任务🧨
-50 0,1 * * * node /scripts/jd_nian_sign.js >> /scripts/logs/jd_nian_sign.log 2>&1
-# 京东炸年兽AR
-50 0,1,8 * * * node /scripts/jd_nian_ar.js >> /scripts/logs/jd_nian_ar.log 2>&1
-# 京东炸年兽小程序
-50 0,1,8 * * * node /scripts/jd_nian_wechat.js >> /scripts/logs/jd_nian_wechat.log 2>&1
-# 京东神仙书院(活动时间:2021-1-20至2021-2-5)
-30 6,8,12,22 * * * node /scripts/jd_immortal.js >> /scripts/logs/jd_immortal.log 2>&1
-# 京东神仙书院答题(活动时间:2021-1-20至2021-2-5)
-5 1 * * * node /scripts/jd_immortal_answer.js >> /scripts/logs/jd_immortal_answer.log 2>&1
-# 5G狂欢城(2021-1-30至2021-2-4)
-0 */6 * * * node /scripts/jd_5g.js >> /scripts/logs/jd_5g.log 2>&1
-0 */6 * * * node /scripts/jd_818.js >> /scripts/logs/jd_818.log 2>&1
+
 # 小鸽有礼(活动时间：2021年1月15日至2021年2月19日)
 5 7 * * * node /scripts/jd_xg.js >> /scripts/logs/jd_xg.log 2>&1
 # 小鸽有礼2(活动时间：2021年1月28日～2021年2月28日)
 34 9 * * * node /scripts/jd_xgyl.js >> /scripts/logs/jd_jd_xgyl.log 2>&1
-# 京东手机年终奖(活动时间：2021年1月26日～2021年2月8日)
-15 0 * * * node /scripts/jd_festival.js >> /scripts/logs/jd_festival.log 2>&1
-# 盲盒抽京豆(活动时间：2021年1月6日～2021年2月5日)
-1 7 * * * node /scripts/jd_mh.js >> /scripts/logs/jd_mh.log 2>&1
 # 京东压岁钱(活动时间：2021-2-1至2021-2-11)
 20 8,12 * * * node /scripts/jd_newYearMoney.js >> /scripts/logs/jd_newYearMoney.log 2>&1
 # 京东压岁钱抢百元卡(活动时间：2021-2-1至2021-2-11)
@@ -37,7 +17,7 @@
 
 ##############长期活动##############
 # 签到
-0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
+3 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
 # 东东超市兑换奖品
 0,30 0 * * * node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
 # 摇京豆
@@ -99,9 +79,7 @@
 # 微信小程序京东赚赚
 10 11 * * * node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1
 # 宠汪汪邀请助力
-10 10,11 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
-# 注销京东已开的店铺会员，不是注销京东plus会员，个别店铺无法注销
-44 4 * * 6 node /scripts/jd_unbind.js >> /scripts/logs/jd_unbind.log 2>&1
+10 9-20/2 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
 # crazyJoy自动每日任务
 10 7 * * * node /scripts/jd_crazy_joy.js >> /scripts/logs/jd_crazy_joy.log 2>&1
 # 京东汽车旅程赛点兑换金豆
@@ -120,9 +98,7 @@
 # */20 * * * * node /scripts/jd_family.js >> /scripts/logs/jd_family.log 2>&1
 # 闪购盲盒
 27 8 * * * node /scripts/jd_sgmh.js >> /scripts/logs/jd_sgmh.log 2>&1
-# 源头好物红包
-0 0 * * *  node /scripts/jd_coupon.js >> /scripts/logs/jd_coupon.log 2>&1
 # 京东秒秒币
 10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
-# 京东超级盒子
-20 7 * * * node /scripts/jd_super_box.js >> /scripts/logs/jd_super_box.log 2>&1
+# 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
+#20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
