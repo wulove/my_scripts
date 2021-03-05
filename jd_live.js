a1b898a35100e9c0b3ff9ddad9c661d0b3448f7a
wulove <iwulove@gmail.com>
2021-03-05 13:45:25 +0800

    update

--- jd_live.js
+++ jd_live.js
@@ -22,9 +22,9 @@ cron "10-20/5 12 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/mas
  */
 const $ = new Env('京东直播');
 
-const notify = $.isNode() ? require('../sendNotify') : '';
+const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
