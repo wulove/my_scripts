2cafc44b716e12f337fedba9c6023c2b38225bcc
wulove <iwulove@gmail.com>
2021-03-20 21:23:53 +0800

    update

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -7,8 +7,8 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PetShareCodes = [
-  '',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'MTAxODc2NTEzNDAwMDAwMDAyMDIwNTMwOQ==@MTAxODcxOTI2NTAwMDAwMDAyMDA1NTg1Nw==',//账号一的好友shareCode,不同好友中间用@符号隔开
+  'MTAxODc2NTEzNDAwMDAwMDAyMDIwNTMwOQ==@MTAxODcxOTI2NTAwMDAwMDAyMDA1NTg1Nw==',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.PETSHARECODES) {
