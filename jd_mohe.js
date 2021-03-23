a691c8ac3c727af66c06b7f57213e2910d6d8f5e
wulove <iwulove@gmail.com>
2021-03-23 09:04:26 +0800

    update

--- jd_mohe.js
+++ jd_mohe.js
@@ -37,7 +37,7 @@ if ($.isNode()) {
 
 const JD_API_HOST = 'https://isp5g.m.jd.com';
 //邀请码一天一变化，已确定
-$.shareId = ['03760a57-1eeb-47db-8591-9bfd8e67f375','67fa1081-f200-401a-8385-b4dc78025b5d'];
+$.shareId = ['7593c0e6-806b-4e16-8d82-011617367acd','7a2248ee-e460-4a81-ac02-8dbf963736b6'];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
