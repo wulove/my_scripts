bbf00c29c50cfcd6f7995838d2ecc12087c00363
wulove <iwulove@gmail.com>
2021-04-04 10:56:39 +0800

    update

--- wait.py
+++ wait.py
@@ -6,14 +6,16 @@ import math
 
 if (len(sys.argv) < 2):
 	exit()
-logging.warning(f'当前时间：{datetime.now()}')
+now = datetime.now()
+zone_cn = timezone(timedelta(hours=8))
+now_cn = now.astimezone(zone_cn)
+logging.warning(f'服务器时间：{now}, 北京时间：{now_cn}')
 target_time = sys.argv[1]
 logging.warning(f'参数值：{target_time}')
-now = datetime.now()
-target_time = datetime.strptime(now.strftime("%Y-%m-%d ") + target_time, "%Y-%m-%d %H:%M:%S.%f")
+target_time = datetime.strptime(now_cn.strftime("%Y-%m-%d ") + target_time, "%Y-%m-%d %H:%M:%S.%f")
 logging.warning(f'目标时间：{target_time}')
 if (datetime.now() < target_time):
-	td = target_time - datetime.now()
+	td = target_time - now_cn
 	logging.warning(f'需要沉睡：{td.seconds}秒')
 	time.sleep(td.seconds+math.floor(td.microseconds/1000)/1000)
 logging.warning(f'当前时间：{datetime.now()}')
