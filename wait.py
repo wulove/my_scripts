1e97dc249534453afab4596dfa0271f9ef9bff50
wulove <iwulove@gmail.com>
2021-04-04 11:06:43 +0800

    update

--- wait.py
+++ wait.py
@@ -10,12 +10,17 @@ now = datetime.now()
 zone_cn = timezone(timedelta(hours=8))
 now_cn = now.astimezone(zone_cn)
 logging.warning(f'服务器时间：{now}, 北京时间：{now_cn}')
-target_time = sys.argv[1]
-logging.warning(f'参数值：{target_time}')
-target_time = datetime.strptime(now_cn.strftime("%Y-%m-%d ") + target_time, "%Y-%m-%d %H:%M:%S.%f")
+logging.warning(f'参数值：{sys.argv[1]}')
+target_time = datetime.strptime(now_cn.strftime("%Y-%m-%d ") + sys.argv[1], "%Y-%m-%d %H:%M:%S.%f")
+if (now_cn.strftime("%H:%M:%S.%f") > sys.argv[1]) {
+	target_time = target_time + timedelta(days=1)
+}
 logging.warning(f'目标时间：{target_time}')
-if (now_cn < target_time):
-	td = target_time - now_cn
-	logging.warning(f'需要沉睡：{td.seconds}秒')
-	time.sleep(td.seconds+math.floor(td.microseconds/1000)/1000)
+td = target_time - now_cn
+# 当目标时间与当前时间相差过大时，直接跳过
+if td.seconds > 3600 * 2:
+	logging.error('当天时间与目标时间相差过大，直接跳过')
+	exit()
+logging.warning(f'需要沉睡：{td.seconds}秒')
+time.sleep(td.seconds+math.floor(td.microseconds/1000)/1000)
 logging.warning(f'当前时间：{datetime.now()}')
