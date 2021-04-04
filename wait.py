1d0a1cbb99f2218a98c8f2e258489b184223b085
wulove <iwulove@gmail.com>
2021-04-04 11:16:52 +0800

    update

--- wait.py
+++ wait.py
@@ -12,9 +12,9 @@ now_cn = now.astimezone(zone_cn)
 logging.warning(f'服务器时间：{now}, 北京时间：{now_cn}')
 logging.warning(f'参数值：{sys.argv[1]}')
 target_time = datetime.strptime(now_cn.strftime("%Y-%m-%d ") + sys.argv[1], "%Y-%m-%d %H:%M:%S.%f")
+# 当前时间在设置时间前，则默认目标时间设置为第二天
 if now_cn.strftime("%H:%M:%S.%f") > sys.argv[1]:
 	target_time = target_time + timedelta(days=1)
-}
 logging.warning(f'目标时间：{target_time}')
 td = target_time - now_cn
 # 当目标时间与当前时间相差过大时，直接跳过
