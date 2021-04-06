a0a4b812f078138b36124f562aa8aea532392b75
wulove <iwulove@gmail.com>
2021-04-06 15:34:31 +0800

    update

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -604,7 +604,7 @@ function taskPostUrl(function_id, body) {
       'Accept-Language': 'zh-cn',
       'Content-Type': 'application/x-www-form-urlencoded',
       'Origin': 'https://lzdz-isv.isvjcloud.com',
-      'User-Agent': 'JD4iPhone/167490 (iPhone; iOS 14.2; Scale/3.00)',
+      'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
       'Referer': `https://lzdz-isv.isvjcloud.com/dingzhi/book/develop/activity?activityId=${ACT_ID}`,
       'Cookie': `${cookie} isvToken=${$.isvToken};`
     }
