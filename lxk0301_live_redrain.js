8eea029596e1bcc60778e886dc55f91716fdea2c
wulove <iwulove@gmail.com>
2021-04-08 08:55:43 +0800

    update

--- lxk0301_live_redrain.js
+++ lxk0301_live_redrain.js
@@ -1,6 +1,6 @@
 /*
 只能用一天。需每天寻找直播抓包
-1 8-23 * * * lxk0301_live_redrain.js
+1 8-23 * * * https://github.com/ljqailym/jd_scripts/blob/master/lxk0301_live_redrain.js
 */
 const $ = new Env('整点京豆雨');
 let allMessage = '';
