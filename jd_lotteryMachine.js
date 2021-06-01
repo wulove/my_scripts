cfb1a11da03555bb46ad30679bce6ececfd124f1
wulove <gmail.com>
2021-06-01 14:07:40 +0800

    update

--- jd_lotteryMachine.js
+++ jd_lotteryMachine.js
@@ -18,9 +18,10 @@ Object.keys(jdCookieNode).forEach((item) => {
 if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
 if (JSON.stringify(process.env).indexOf('GITHUB') > -1) process.exit(0);
 
-const appIdArr = ['1EFRRxA','1EFRQwA','1EFRYxQ','1EFRXxg','1EFVRwA','1EFVRxw','1EFRZwA','1EFRZwQ','1EFRYwA'];
-const homeDataFunPrefixArr = ['interact_template','interact_template','harmony_template','','','','','','','','','','','','','','','interact_template','interact_template'];
-const collectScoreFunPrefixArr = ['','','','','','','','','','','','','','','','','','interact_template','interact_template'];
+const appIdArr = ['1EFRRxA','1EFRQwA','1EFRYxQ','1EFRXxg','1EFVRwA','1EFVRxw','1EFRZwA','1EFRZwQ','1EFRYwA','1EFVRxg','1EFVRxQ']
+const homeDataFunPrefixArr = ['interact_template','interact_template','harmony_template','','','','','','','','','','','','','','','interact_template','interact_template']
+const collectScoreFunPrefixArr = ['','','','','','','','','','','','','','','','','','interact_template','interact_template']
+
 $.allShareId = {};
 main();
 async function main() {
@@ -196,7 +197,7 @@ function harmony_collectScore(timeout = 0) {
           data = JSON.parse(data);
           if (data['code'] === 0) {
             if (data['data']['bizCode'] === 0) {
-              console.log(`助力结果：${data.data.bizMsg}\n`);
+              console.log(`助力结果：${data.data.bizMsg}🎉\n`);
             } else {
               if (data['data']['bizCode'] === 108) $.canHelp = false;
               if (data['data']['bizCode'] === 103) $.item['max'] = true;
