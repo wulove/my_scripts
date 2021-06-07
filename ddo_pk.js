a95a41f02ec7e88da7930a03fcb7a6ef2039d4f1
wulove <gmail.com>
2021-06-07 09:09:45 +0800

    update

--- ddo_pk.js
+++ ddo_pk.js
@@ -1,11 +1,22 @@
 /*
 京享值PK
+cron 15 0,6,13,19,21 * * * ddo_pk.js
 更新时间：2021-6-4
 活动入口：京东APP-我的-京享值
-由于服务器压力过大，已删除默认cron，请自行设置
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
- */
+============Quantumultx===============
+[task_local]
+#京享值PK
+15 0,6,13,19,21 * * * https://raw.githubusercontent.com/hyzaw/scripts/main/ddo_pk.js, tag=京享值PK
+================Loon==============
+[Script]
+cron "15 0,6,13,19,21 * * *" script-path=https://raw.githubusercontent.com/hyzaw/scripts/main/ddo_pk.js,tag=京享值PK
+===============Surge=================
+京享值PK = type=cron,cronexp="15 0,6,13,19,21 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/hyzaw/scripts/main/ddo_pk.js
+============小火箭=========
+京享值PK = type=cron,script-path=https://raw.githubusercontent.com/hyzaw/scripts/main/ddo_pk.js, cronexpr="15 0,6,13,19,21 * * *", timeout=3600, enable=true
+*/
 const $ = new Env('京享值PK');
 $.toObj = (t, e = null) => {
 	try {
@@ -136,7 +147,7 @@ function submitPKCode (pin) {
 	console.log(`上传pk码: ${pin}`);
 	return new Promise((resolve) => {
 		let options = {
-			"url": `https://pool.nz.lu/upload/PKv2/${pin}`,
+			"url": `https://pool.nz.lu/api/v2/upload?name=PK&code=${pin}`,
 			"headers": {
 				"Host": "pool.nz.lu",
 				"Connection": "keep-alive",
@@ -164,7 +175,7 @@ function getPinList(num = 20){
 	console.log("获取Pk列表");
 	return new Promise((resolve) => {
 		let options = {
-			"url": `https://pool.nz.lu/get/PKv2/${num}`,
+			"url": `https://pool.nz.lu/api/v2/get?name=PK&count=${num}`,
 			"headers": {
 				"Host": "pool.nz.lu",
 				"Connection": "keep-alive",
