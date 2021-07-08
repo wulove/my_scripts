21ef2606e79575507d857783f1e6fb07fd1c4ddd
wulove <gmail.com>
2021-07-08 15:48:12 +0800

    update
    Signed-off-by: wulove <gmail.com>

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -67,7 +67,7 @@ if ($.isNode()) {
       try {
         // await jdMh('https://h5.m.jd.com/babelDiy/Zeus/3eeruLXVbXge6CexVq8XkBbBvAfy/index.html')
         await jdMh('https://anmp.jd.com/babelDiy/Zeus/9Z9HZWmQuwDnYKC1rzZTMpdn8ms/index.html')
-        // await jdMh('https://anmp.jd.com/babelDiy/Zeus/yiNQjMxQvs3R3SdS4nwa2MFk1FE/index.html?wxAppName=jd')
+        await jdMh('https://anmp.jd.com/babelDiy/Zeus/2e6SGdSEVmY18TSBeu7ewRKHmp22/index.html?wxAppName=jd')
       } catch (e) {
         $.logErr(e)
       }
