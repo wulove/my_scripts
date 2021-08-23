64401b804c1b8d7acffbcec496a25ddf961c8a0f
wulove <gmail.com>
2021-08-23 08:59:33 +0800

    update
    Signed-off-by: wulove <gmail.com>

--- jd_wish.js
+++ jd_wish.js
@@ -68,13 +68,7 @@ if ($.isNode()) {
       }
     }
   }
-  let res = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/wish.json')
-  if (!res) {
-    $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/wish.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
-    await $.wait(1000)
-    res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/wish.json')
-  }
-  $.shareCode = [...$.shareCode, ...(res || [])]
+
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
