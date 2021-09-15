804fc84237cbe9dcf4191e116dbcd0652d62b725
wulove <gmail.com>
2021-09-15 10:42:32 +0800

    update
    Signed-off-by: wulove <gmail.com>

--- jd_cash.js
+++ jd_cash.js
@@ -87,7 +87,7 @@ async function jdCash() {
   await index()
 
   await shareCodesFormat()
-  // await helpFriends()
+  await helpFriends()
   await getReward()
   await getReward('2');
   $.exchangeBeanNum = 0;
