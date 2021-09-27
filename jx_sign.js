6e8397fe0951bcc97371587c3491a73dc85ac5f7
wulove <gmail.com>
2021-09-27 15:23:43 +0800

    update
    Signed-off-by: wulove <gmail.com>

--- jx_sign.js
+++ jx_sign.js
@@ -37,7 +37,7 @@ let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭�
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 let UA, UAInfo = {};
-$.shareCodes = [];
+$.shareCodes = [{'smp':'7a3c29871e087342d38603bb81738544','num':0,'max':0},{'smp':'bf45dbbf13be83c1ea2c691023d6135b','num':0,'max':0}];
 $.blackInfo = {}
 $.appId = 10028;
 if ($.isNode()) {
