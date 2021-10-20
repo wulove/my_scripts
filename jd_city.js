09a1e9c71e0adee5cba0804a1026f32e59899bb3
wulove <gmail.com>
2021-10-20 15:52:13 +0800

    update
    Signed-off-by: wulove <gmail.com>

--- jd_city.js
+++ jd_city.js
@@ -272,7 +272,7 @@ function readShareCode() {
       }
     })
     await $.wait(10000);*/
-    resolve(inviteCodes)
+    resolve({code: 200, data: inviteCodes})
   })
 }
 //格式化助力码
@@ -280,13 +280,6 @@ function shareCodesFormat() {
   return new Promise(async resolve => {
     // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
     $.newShareCodes = [];
-    if ($.shareCodesArr[$.index - 1]) {
-      $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
-    } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
-      const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
-      $.newShareCodes = inviteCodes[tempIndex].split('@');
-    }
     const readShareCodeRes = await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
