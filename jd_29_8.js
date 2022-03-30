669e7938d744f1a87f792377b4430b0bb7d9397b
wulove <gmail.com>
2022-03-30 14:54:30 +0800

    update

--- jd_29_8.js
+++ jd_29_8.js
@@ -52,6 +52,7 @@ let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
         message = '';
         let t = moment().format("mmss.SSS")
         if (t > 5959.900) {
+          console.log("开始连续执行")
           for (let k=0; k < 6; k++) {
             qiang();
             // await $.wait(10 + Math.random() * 10)
