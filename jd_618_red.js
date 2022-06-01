7dc2e976be80772436341ebea7673d935db5aaf9
wulove <gmail.com>
2022-06-01 16:40:55 +0800

    update

--- jd_618_red.js
+++ jd_618_red.js
@@ -52,7 +52,7 @@ let appId, fingerprint, token, enCryptMethodJD;
 })().catch((e) => { $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '') }).finally(() => { $.done(); })
 
 async function main(ck, code) {
-    const codes = ['ICuFjBQ','IdsKAD6','ILsthA1']
+    const codes = ['lCwkjLB', "lMw7Td2", "ltwk41D"]
     code = $.CODE618 ? $.CODE618 : codes[random(0, codes.length)]
     //console.log(code)
     let userName = decodeURIComponent(ck.match(/pt_pin=([^; ]+)(?=;?)/) && ck.match(/pt_pin=([^; ]+)(?=;?)/)[1])
